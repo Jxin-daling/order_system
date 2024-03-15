a3ad37c9e26e5183d493446c1da9299817695093
Jxin_darling <3167044821@qq.com>
2024-03-15 23:32:58 +0800

    订单列表页面

--- 小程序/pages/order/list/list.js
+++ 小程序/pages/order/list/list.js
@@ -1,66 +1,82 @@
-// pages/order/list/list.js
+const fetch = require('../../../utils/fetch')
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    orderlist:[],
+    is_last:false  //判断下拉是否完成
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+  last_id:0,  //加载文件的标识
 
+  // 定义封装请求方法
+  loadData(data,success,fail){
+    data.row = 10  //一次获取十条数据
+    fetch('food/orderlist',data).then((res)=>{
+      this.last_id = res.data.last_id
+      this.setData({
+        is_last:this.data.is_last
+      },()=>{
+        success(res.data)
+      })
+    },fail)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  // 查看详情跳转
+  orderdetail(e){
+    console.log(e);
+    wx.navigateTo({
+      url: '../detail/detail?order_id='+e.currentTarget.dataset.postid,
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  // 初始化
+  onLoad(){
+    wx.showLoading({
+      title: '加载中',
+    })
+    this.loadData({last_id:0},(data)=>{
+      this.setData({
+        orderlist:data.list
+      },()=>{
+        wx.hideLoading()
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  // 下拉刷新
+  PullDownRefresh(){
+    wx.showNavigationBarLoading()
+    this.loadData({last_id:0},(data)=>{
+      this.setData({
+        orderlist:data.list
+      },()=>{
+        wx.hideNavigationBarLoading()
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
+  // 上拉触底
+  onReachBottom(){
+    // 判断是否还有数据，无则不执行请求
+    if(this.data.is_last){
+      return
+    }
+    
+    wx.showLoading({
+      title: '正在加载中'
+    })
 
-  },
+    let orderlist = this.data.orderlist
+    this.loadData({last_id:this.last_id},(data)=>{
+      for(let i = 0; i < data.list.length; i++){
+        orderlist.push(data.list[i])
+      }
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+      this.setData({
+        orderlist:this.data.orderlist
+      },()=>{
+        wx.hideLoading()
+      })
+    })
   }
 })
